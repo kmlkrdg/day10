a8deb31f74e901f82fd2a382d3b7c5debad2b115
kmlkrdg <kemalkaradag52@gmail.com>
2023-02-17 09:35:29 +0300

    day10

---
+++ srp.ts
@@ -0,0 +1,29 @@
+
+class ProductRepository {
+    productList: Product[] = [];
+
+    add(product: Product) {
+        this.productList.push(product);
+    }
+    remove(id: number) {
+        //this.productList.slice()
+    }
+    update(product: Product) {
+
+    }
+}
+class ProductPresenter {
+    writeToConsole(productList: Product[]) {
+        productList.forEach(x => {
+            console.log(x);
+        })
+    }
+}
+
+
+class Product {
+    id: number;
+    namer: string;
+}
+
+
